4f66aebd8cba1e7717bf11d5500928a7c74e914d
Stan Bennett <stanley.bennett82@gmail.com>
2022-02-10 22:19:21 -0600

    Add WindowsApp

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -29,9 +28,9 @@ namespace GameSpace
             DriveInfo driveInfo = new("D:\\");
 
             Console.WriteLine();
-            Console.WriteLine("         Total Space: {0}", FormatBytes(driveInfo.TotalSize));
-            Console.WriteLine("          Used Space: {0}", FormatBytes(driveInfo.TotalSize - driveInfo.AvailableFreeSpace));
-            Console.WriteLine("Available Free Space: {0}", FormatBytes(driveInfo.AvailableFreeSpace));
+            Console.WriteLine("         Total Space: {0}", ByteFormatter.Format(driveInfo.TotalSize));
+            Console.WriteLine("          Used Space: {0}", ByteFormatter.Format(driveInfo.TotalSize - driveInfo.AvailableFreeSpace));
+            Console.WriteLine("Available Free Space: {0}", ByteFormatter.Format(driveInfo.AvailableFreeSpace));
             Console.WriteLine();
 
             List<Task<GameInfo>> tasks = new();
@@ -44,10 +43,10 @@ namespace GameSpace
             });
 
             GameInfo[] games = await Task.WhenAll(tasks.ToArray());
-            foreach (GameInfo gameInfo in games.OrderByDescending(g => g.GameSize))
+            foreach (GameInfo gameInfo in games.OrderByDescending(g => g.GameSizeBytes))
             {
                 int leftPadding = games.Max(g => g.Name.Length) + 10;
-                Console.WriteLine("{0}: {1} [{2}]", gameInfo.Name.PadLeft(leftPadding), FormatBytes(gameInfo.GameSize).PadRight(10), GetPercentage(gameInfo.GameSize, driveInfo.TotalSize));
+                Console.WriteLine("{0}: {1} [{2}]", gameInfo.Name.PadLeft(leftPadding), gameInfo.GameSizeFormatted.PadRight(10), GetPercentage(gameInfo.GameSizeBytes, driveInfo.TotalSize));
             }
 
             Console.WriteLine();
@@ -69,29 +68,17 @@ namespace GameSpace
             return Task.FromResult(new GameInfo(dir.Name, totalSize));
         }
 
-        private static string FormatBytes(long bytes)
-        {
-            string[] suffix = { "B", "KB", "MB", "GB", "TB" };
-            int i;
-            double dblSByte = bytes;
-            for (i = 0; i < suffix.Length && bytes >= 1024; i++, bytes /= 1024)
-            {
-                dblSByte = bytes / 1024.0;
-            }
-
-            return $"{dblSByte:0.##} {suffix[i]}";
-        }
-
         private static string[] GetGameRootDirectories()
         {
             List<string> directories = new()
             {
-                @"D:\Battlet_Net",
+                @"D:\Battle_Net",
                 @"D:\Epic",
                 @"D:\GOG_Galaxy\Games",
                 @"D:\origin_games",
                 @"D:\Steam\steamapps\common",
-                @"D:\Ubisoft_Connect"
+                @"D:\Ubisoft_Connect",
+                @"D:\WindowsApps"
             };
 
             return directories.ToArray();
@@ -152,17 +139,41 @@ namespace GameSpace
 
         #region Helper Classes
 
+        private static class ByteFormatter
+        {
+
+            public static string Format(long bytes)
+            {
+                string[] suffix = { "B", "KB", "MB", "GB", "TB" };
+                int i;
+                double dblSByte = bytes;
+                for (i = 0; i < suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+                {
+                    dblSByte = bytes / 1024.0;
+                }
+
+                return $"{dblSByte:0.##} {suffix[i]}";
+            }
+
+        }
+
         private class GameInfo
         {
 
             public GameInfo(string name, long gameSize)
             {
                 Name = name;
-                GameSize = gameSize;
+                GameSizeBytes = gameSize;
             }
 
-            public long GameSize { get; private set; }
+            public long GameSizeBytes { get; private set; }
+            public string GameSizeFormatted => ByteFormatter.Format(GameSizeBytes);
             public string Name { get; private set; }
+
+            public override string ToString()
+            {
+                return $"{Name} [{GameSizeFormatted}]";
+            }
 
         }
 
